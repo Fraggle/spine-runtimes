069c0076572fc06f593245b145f621a64458d02c
Mathieu Garaud <mathieu.garaud@gmail.com>
2018-04-25 18:27:43 +0200

    Add tool to spot textures with higher or lower resolution than visible size
    https://aras-p.info/blog/2011/05/03/a-way-to-visualize-mip-levels/

--- spine-cocos2dx/src/spine/SkeletonTwoColorBatch.cpp
+++ spine-cocos2dx/src/spine/SkeletonTwoColorBatch.cpp
@@ -47,9 +47,11 @@ TwoColorTrianglesCommand::TwoColorTrianglesCommand() :_materialID(0), _textureID
 	func = [this]() { draw(); };
 }
 
-void TwoColorTrianglesCommand::init(float globalOrder, GLuint textureID, GLProgramState* glProgramState, BlendFunc blendType, const TwoColorTriangles& triangles, const Mat4& mv, uint32_t flags) {
+void TwoColorTrianglesCommand::init(float globalOrder, cocos2d::Texture2D* texture, GLProgramState* glProgramState, BlendFunc blendType, const TwoColorTriangles& triangles, const Mat4& mv, uint32_t flags) {
 	CCASSERT(glProgramState, "Invalid GLProgramState");
 	CCASSERT(glProgramState->getVertexAttribsFlags() == 0, "No custom attributes are supported in QuadCommand");
+
+    GLuint const textureID = texture->getName();
 
 	RenderCommand::init(globalOrder, mv, flags);
 
@@ -71,6 +73,11 @@ void TwoColorTrianglesCommand::init(float globalOrder, GLuint textureID, GLProgr
 
 		generateMaterialID();
 	}
+
+#ifdef DEBUG_TEXTURE_SIZE
+    CC_ASSERT(texture != nullptr);
+    _texSize = { static_cast<float>(texture->getPixelsWide()), static_cast<float>(texture->getPixelsHigh()) };
+#endif
 }
 
 TwoColorTrianglesCommand::~TwoColorTrianglesCommand() {
@@ -83,7 +90,7 @@ void TwoColorTrianglesCommand::generateMaterialID() {
 		setSkipBatching(true);
 	}
 	else {
-		int glProgram = (int)_glProgram->getProgram();		
+		int glProgram = (int)_glProgram->getProgram();
 		_materialID = glProgram + (int)_textureID + (int)_blendType.src + (int)_blendType.dst;
 	}
 }
@@ -91,92 +98,146 @@ void TwoColorTrianglesCommand::generateMaterialID() {
 void TwoColorTrianglesCommand::useMaterial() const {
 	//Set texture
 	GL::bindTexture2D(_textureID);
-	
+
 	if (_alphaTextureID > 0) {
 		// ANDROID ETC1 ALPHA supports.
 		GL::bindTexture2DN(1, _alphaTextureID);
 	}
 	//set blend mode
 	GL::blendFunc(_blendType.src, _blendType.dst);
-	
+
+#ifdef DEBUG_TEXTURE_SIZE
+    CC_ASSERT(_texSize != Vec2::ZERO);
+    CC_ASSERT(_glProgramState != nullptr);
+    _glProgramState->setUniformVec2(GLProgram::UNIFORM_NAME_TEX_SIZE, _texSize);
+#endif
+
 	_glProgramState->apply(_mv);
 }
-	
+
 void TwoColorTrianglesCommand::draw() {
 	SkeletonTwoColorBatch::getInstance()->batch(this);
 }
 
-const char* TWO_COLOR_TINT_VERTEX_SHADER = STRINGIFY(
+const char* TWO_COLOR_TINT_VERTEX_SHADER = R"(
+#ifdef GL_ES
+attribute mediump vec4 a_position;
+attribute lowp vec4 a_color;
+attribute lowp vec4 a_color2;
+attribute lowp vec2 a_texCoords;
+
+varying lowp vec4 v_light;
+varying lowp vec4 v_dark;
+varying lowp vec2 v_texCoord;
+varying mediump vec2 v_mipTexCoord; // DEBUG_TEXTURE_SIZE
+#else
 attribute vec4 a_position;
 attribute vec4 a_color;
 attribute vec4 a_color2;
 attribute vec2 a_texCoords;
 
-\n#ifdef GL_ES\n
+varying vec4 v_light;
+varying vec4 v_dark;
+varying vec2 v_texCoord;
+varying vec2 v_mipTexCoord; // DEBUG_TEXTURE_SIZE
+#endif
+
+void main()
+{
+    v_light = a_color;
+    v_dark = a_color2;
+    v_texCoord = a_texCoords;
+#ifdef DEBUG_TEXTURE_SIZE
+    v_mipTexCoord = a_texCoords * (u_TexSize / 8.0);
+#endif
+    gl_Position = CC_PMatrix * a_position;
+}
+
+)";
+
+const char* TWO_COLOR_TINT_FRAGMENT_SHADER = R"(
+#ifdef GL_ES
+precision lowp float;
+
 varying lowp vec4 v_light;
 varying lowp vec4 v_dark;
-varying mediump vec2 v_texCoord;
-\n#else\n
+varying lowp vec2 v_texCoord;
+varying mediump vec2 v_mipTexCoord; // DEBUG_TEXTURE_SIZE
+#else
 varying vec4 v_light;
 varying vec4 v_dark;
 varying vec2 v_texCoord;
+varying vec2 v_mipTexCoord; // DEBUG_TEXTURE_SIZE
+#endif
 
-\n#endif\n
-
-void main() {
-	v_light = a_color;
-	v_dark = a_color2;
-	v_texCoord = a_texCoords;
-	gl_Position = CC_PMatrix * a_position;
+void main()
+{
+#ifdef DEBUG_TEXTURE_SIZE
+    if (CC_Debug == 1)
+    {
+        vec4 texColor = texture2D(CC_Texture0, v_texCoord);
+        float alpha = texColor.a * v_light.a;
+        vec4 col = vec4(((texColor.a - 1.0) * v_dark.a + 1.0 - texColor.rgb) * v_dark.rgb + texColor.rgb * v_light.rgb, alpha);
+        vec4 mip = texture2D(CC_Texture3, v_mipTexCoord);
+        gl_FragColor = vec4(mix(col.rgb, mip.rgb, mip.a).rgb, col.a);
+    }
+    else
+#endif
+    {
+        vec4 texColor = texture2D(CC_Texture0, v_texCoord);
+        float alpha = texColor.a * v_light.a;
+        gl_FragColor = vec4(((texColor.a - 1.0) * v_dark.a + 1.0 - texColor.rgb) * v_dark.rgb + texColor.rgb * v_light.rgb, alpha);
+    }
 }
-);
 
-const char* TWO_COLOR_TINT_FRAGMENT_SHADER = STRINGIFY(
-\n#ifdef GL_ES\n
-precision lowp float;
-\n#endif\n
-
-varying vec4 v_light;
-varying vec4 v_dark;
-varying vec2 v_texCoord;
-
-void main() {
-	vec4 texColor = texture2D(CC_Texture0, v_texCoord);
-	float alpha = texColor.a * v_light.a;
-	gl_FragColor.a = alpha;	
-	gl_FragColor.rgb = ((texColor.a - 1.0) * v_dark.a + 1.0 - texColor.rgb) * v_dark.rgb + texColor.rgb * v_light.rgb;
-}
-);
+)";
 
 /** PRETTY SIMPLE ADDITION **/
 
 // the above shader would not work properlly with pre-multiplied alpha textures
-    
-const char* TWO_COLOR_TINT_PMA_FRAGMENT_SHADER = STRINGIFY(
-\n#ifdef GL_ES\n
-precision highp float;
-\n#endif\n
-                                                           
-varying vec4 v_light;   // a pma value (as skeletonRenderer opacityModifyRgb() must be true to work properly with pma)
-varying vec4 v_dark;    // a non-pma value (dark alpha should be one anyway)
+
+const char* TWO_COLOR_TINT_PMA_FRAGMENT_SHADER = R"(
+#ifdef GL_ES
+precision lowp float;
+
+varying lowp vec4 v_light; // a pma value (as skeletonRenderer opacityModifyRgb() must be true to work properly with pma)
+varying lowp vec4 v_dark; // a non-pma value (dark alpha should be one anyway)
+varying lowp vec2 v_texCoord;
+varying mediump vec2 v_mipTexCoord; // DEBUG_TEXTURE_SIZE
+#else
+varying vec4 v_light;
+varying vec4 v_dark;
 varying vec2 v_texCoord;
-                                                           
-void main() {
-    vec4 texColor = texture2D(CC_Texture0, v_texCoord);     // input PMA texture
+varying vec2 v_mipTexCoord; // DEBUG_TEXTURE_SIZE
+#endif
 
-    float alpha = texColor.a * v_light.a;   // new alpha will be texture alpha * light.alpha
-
-    gl_FragColor.a = alpha;
-    
-    float q = ((( texColor.a - 1.0 ) * v_dark.a) + 1.0);
-    
-    gl_FragColor.rgb = clamp((q * texColor.a - texColor.rgb) * v_dark.rgb * v_light.a + (texColor.rgb * v_light.rgb), 0.0, 1.0);
+void main()
+{
+#ifdef DEBUG_TEXTURE_SIZE
+    if (CC_Debug == 1)
+    {
+        vec4 texColor = texture2D(CC_Texture0, v_texCoord);
+        float alpha = texColor.a * v_light.a;
+        float q = (((texColor.a - 1.0) * v_dark.a) + 1.0);
+        vec4 col = vec4(clamp((q * texColor.a - texColor.rgb) * v_dark.rgb * v_light.a + (texColor.rgb * v_light.rgb), 0.0, 1.0), alpha);
+        vec4 mip = texture2D(CC_Texture3, v_mipTexCoord);
+        gl_FragColor = vec4(mix(col.rgb, mip.rgb, mip.a).rgb, col.a);
+    }
+    else
+#endif
+    {
+        vec4 texColor = texture2D(CC_Texture0, v_texCoord); // input PMA texture
+        float alpha = texColor.a * v_light.a; // new alpha will be texture alpha * light.alpha
+        float q = (((texColor.a - 1.0) * v_dark.a) + 1.0);
+        gl_FragColor = vec4(clamp((q * texColor.a - texColor.rgb) * v_dark.rgb * v_light.a + (texColor.rgb * v_light.rgb), 0.0, 1.0), alpha);
+    }
 }
-);
-    
+
+)";
+
 //-----------
-    
-    
+
+
 static SkeletonTwoColorBatch* instance = nullptr;
 
 SkeletonTwoColorBatch* SkeletonTwoColorBatch::getInstance () {
@@ -215,15 +276,15 @@ SkeletonTwoColorBatch::SkeletonTwoColorBatch ()
 	Director::getInstance()->getEventDispatcher()->addCustomEventListener(EVENT_AFTER_DRAW_RESET_POSITION, [this](EventCustom* eventCustom){
 		this->update(0);
 	});;
-	
+
     init();
 }
 
 SkeletonTwoColorBatch::~SkeletonTwoColorBatch () {
 	Director::getInstance()->getEventDispatcher()->removeCustomEventListeners(EVENT_AFTER_DRAW_RESET_POSITION);
-	
+
 	spUnsignedShortArray_dispose(_indices);
-	
+
 	for (unsigned int i = 0; i < _commandsPool.size(); i++) {
 		delete _commandsPool[i];
 		_commandsPool[i] = nullptr;
@@ -233,7 +294,7 @@ SkeletonTwoColorBatch::~SkeletonTwoColorBatch () {
 	delete _indexBuffer;
 }
 
-void SkeletonTwoColorBatch::update (float delta) {	
+void SkeletonTwoColorBatch::update (float delta) {
 	reset();
 }
 
@@ -307,13 +368,13 @@ V3F_C4B_C4B_T2F* SkeletonTwoColorBatch::allocateVertices(uint32_t numVertices) {
 			triangles.verts = newData + (triangles.verts - oldData);
 		}
 	}
-	
+
 	V3F_C4B_C4B_T2F* vertices = _vertices.data() + _numVertices;
 	_numVertices += numVertices;
 	return vertices;
 }
-	
-	
+
+
 void SkeletonTwoColorBatch::deallocateVertices(uint32_t numVertices) {
 	_numVertices -= numVertices;
 }
@@ -333,7 +394,7 @@ unsigned short* SkeletonTwoColorBatch::allocateIndices(uint32_t numIndices) {
 			}
 		}
 	}
-	
+
 	unsigned short* indices = _indices->items + _indices->size;
 	_indices->size += numIndices;
 	return indices;
@@ -343,71 +404,71 @@ void SkeletonTwoColorBatch::deallocateIndices(uint32_t numIndices) {
 	_indices->size -= numIndices;
 }
 
-TwoColorTrianglesCommand* SkeletonTwoColorBatch::addCommand(cocos2d::Renderer* renderer, float globalOrder, GLuint textureID, cocos2d::GLProgramState* glProgramState, cocos2d::BlendFunc blendType, const TwoColorTriangles& triangles, const cocos2d::Mat4& mv, uint32_t flags) {
+TwoColorTrianglesCommand* SkeletonTwoColorBatch::addCommand(cocos2d::Renderer* renderer, float globalOrder, cocos2d::Texture2D* texture, cocos2d::GLProgramState* glProgramState, cocos2d::BlendFunc blendType, const TwoColorTriangles& triangles, const cocos2d::Mat4& mv, uint32_t flags) {
 	TwoColorTrianglesCommand* command = nextFreeCommand();
-	command->init(globalOrder, textureID, glProgramState, blendType, triangles, mv, flags);
-	renderer->addCommand(command);	
+	command->init(globalOrder, texture, glProgramState, blendType, triangles, mv, flags);
+	renderer->addCommand(command);
 	return command;
 }
-	
+
 void SkeletonTwoColorBatch::batch (TwoColorTrianglesCommand* command) {
 	if (_numVerticesBuffer + command->getTriangles().vertCount >= MAX_VERTICES || _numIndicesBuffer + command->getTriangles().indexCount >= MAX_INDICES) {
 		flush(_lastCommand);
 	}
-	
+
 	uint32_t materialID = command->getMaterialID();
 	if (_lastCommand && _lastCommand->getMaterialID() != materialID) {
 		flush(_lastCommand);
 	}
-	
+
 	memcpy(_vertexBuffer + _numVerticesBuffer, command->getTriangles().verts, sizeof(V3F_C4B_C4B_T2F) * command->getTriangles().vertCount);
 	const Mat4& modelView = command->getModelView();
 	for (int i = _numVerticesBuffer; i < _numVerticesBuffer + command->getTriangles().vertCount; i++) {
 		modelView.transformPoint(&_vertexBuffer[i].position);
 	}
-	
+
 	unsigned short vertexOffset = (unsigned short)_numVerticesBuffer;
 	unsigned short* indices = command->getTriangles().indices;
 	for (int i = 0, j = _numIndicesBuffer; i < command->getTriangles().indexCount; i++, j++) {
 		_indexBuffer[j] = indices[i] + vertexOffset;
 	}
-	
+
 	_numVerticesBuffer += command->getTriangles().vertCount;
 	_numIndicesBuffer += command->getTriangles().indexCount;
-	
+
 	if (command->isForceFlush()) {
 		flush(command);
 	}
 	_lastCommand = command;
 }
-	
+
 void SkeletonTwoColorBatch::flush (TwoColorTrianglesCommand* materialCommand) {
 	if (!materialCommand)
 		return;
-	
+
 	materialCommand->useMaterial();
-	
+
 	glBindBuffer(GL_ARRAY_BUFFER, _vertexBufferHandle);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(V3F_C4B_C4B_T2F) * _numVerticesBuffer , _vertexBuffer, GL_DYNAMIC_DRAW);
-	
+
 	glEnableVertexAttribArray(_positionAttributeLocation);
 	glEnableVertexAttribArray(_colorAttributeLocation);
 	glEnableVertexAttribArray(_color2AttributeLocation);
 	glEnableVertexAttribArray(_texCoordsAttributeLocation);
-	
+
 	glVertexAttribPointer(_positionAttributeLocation, 3, GL_FLOAT, GL_FALSE, sizeof(V3F_C4B_C4B_T2F), (GLvoid*)offsetof(V3F_C4B_C4B_T2F, position));
 	glVertexAttribPointer(_colorAttributeLocation, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(V3F_C4B_C4B_T2F), (GLvoid*)offsetof(V3F_C4B_C4B_T2F, color));
 	glVertexAttribPointer(_color2AttributeLocation, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(V3F_C4B_C4B_T2F), (GLvoid*)offsetof(V3F_C4B_C4B_T2F, color2));
 	glVertexAttribPointer(_texCoordsAttributeLocation, 2, GL_FLOAT, GL_FALSE, sizeof(V3F_C4B_C4B_T2F), (GLvoid*)offsetof(V3F_C4B_C4B_T2F, texCoords));
-	
+
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBufferHandle);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned short) * _numIndicesBuffer, _indexBuffer, GL_STATIC_DRAW);
-	
+
 	glDrawElements(GL_TRIANGLES, (GLsizei)_numIndicesBuffer, GL_UNSIGNED_SHORT, 0);
-	
+
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	
+
 	_numVerticesBuffer = 0;
 	_numIndicesBuffer = 0;
 	_numBatches++;
